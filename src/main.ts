2099b7b9116a73a14d84f9b39275c6f214356d6c
Ajibare <ajibare.jide@gmail.com>
2026-07-10 23:59:50 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: [
+  app.enableCors({ origin: ['https://bistack-management-backend.vercel.app/',
     'https://bigstack-management.vercel.app/',
      'http://localhost:3000'], });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
